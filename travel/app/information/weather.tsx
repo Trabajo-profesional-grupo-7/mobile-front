3a2fdd62d5cf35cb1947fab6c513ecb2db5213d2
PMartn <71846491+PMartn@users.noreply.github.com>
2024-04-02 14:51:16 -0300

    Weather screen

--- travel/app/information/weather.tsx
+++ travel/app/information/weather.tsx
@@ -4,7 +4,7 @@ import EditScreenInfo from '@/components/EditScreenInfo';
 import { View } from '@/components/Themed';
 import Colors from '@/constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
-import React from 'react';
+import React, { useState } from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useRouter } from 'expo-router';
 const windowWidth = Dimensions.get('window').width;
@@ -12,20 +12,24 @@ const windowHeight = Dimensions.get('window').height;
 const colors = Colors.light;
 
 export default function Weather() {
+  const [location, setLocation] = useState("Rome, Italy");
 
   return (
-    <>
-      
-    </>
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Weather for {location}
+      </Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding:20
   },
   title: {
-    fontSize: 20,
+    fontSize: 35,
     fontWeight: 'bold',
   }
 });
