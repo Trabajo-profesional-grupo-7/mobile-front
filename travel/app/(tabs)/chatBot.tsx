bb01baf9563c6367bdd52d7d8643e76756ff83e0
PMartn <71846491+PMartn@users.noreply.github.com>
2024-04-18 15:22:49 -0300

    Chat screen

--- travel/app/(tabs)/chatBot.tsx
+++ travel/app/(tabs)/chatBot.tsx
@@ -1,21 +1,51 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, TextInput, ScrollView } from "react-native";
 import Colors from '../../constants/Colors';
 import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
 const colors = Colors.light;
 
+
+const ChatMessage = ({ message, isSender }:{message:string,isSender:boolean}) => {
+    return (
+      <View style={[styles.messageContainer, isSender ? styles.senderMessage : styles.receiverMessage]}>
+        <Text style={styles.messageText}>{message}</Text>
+      </View>
+    );
+  };
+
 export default function ChatBot() {
+    const [text, setText] = useState("");
+    const [messages, setMessages] = useState([
+        { message: 'Hi there!', isSender: false },
+        { message: 'Hello!', isSender: true },
+        { message: 'How are you?', isSender: false },
+        { message: 'I am good, thanks!', isSender: true },
+    ])
+
+
+    const sendMessage = () => {
+        setMessages(messages.concat({message:text,isSender:true}))
+        setText("")
+    }
+
     return (
         <View style={styles.container}>
-            <View style={{ flex: 1 }}>
-                <Text>Aca va el chat</Text>
-            </View>
+            <ScrollView style={{ flex: 1 }}>
+                <View style={{paddingHorizontal:15, paddingVertical:5}}>
+                    {messages.map(({ message, isSender }, index) => (
+                        <ChatMessage key={index} message={message} isSender={isSender} />
+                    ))}
+                </View>
+            </ScrollView>
             <View style={styles.inputContainer}>
                 <TextInput
+                    value={text}
+                    onChangeText={setText}
                     placeholder="Type a message..."
                     style={styles.input}
                     multiline
                 />
-                <Ionicons name='send-outline' size={30} style={{ borderRadius: 50, backgroundColor: colors.background, padding: 10, margin: 10 }} />
+                <Ionicons name='send-outline' onPress={sendMessage} size={30} style={{ borderRadius: 50, backgroundColor: colors.background, padding: 10, margin: 10 }} />
             </View>
         </View>
     )
@@ -44,6 +74,24 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10, 
         borderTopColor: colors.secondary, 
         borderTopWidth: 3
-    }
+    },
+    messageContainer: {
+        maxWidth: '70%',
+        marginBottom: 8,
+        padding: 12,
+        borderRadius: 8,
+      },
+      senderMessage: {
+        alignSelf: 'flex-end',
+        backgroundColor: colors.primary,
+      },
+      receiverMessage: {
+        alignSelf: 'flex-start',
+        backgroundColor: '#9ec3ff', 
+      },
+      messageText: {
+        fontSize: 16,
+        color: '#333333',
+      },
 
 })
