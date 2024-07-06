001506a7f14c5b4045c35a42da198fa0abb918e1
PMartn <71846491+PMartn@users.noreply.github.com>
2024-07-06 11:04:07 -0300

    Notifications

--- travel/app/context/AuthContext.tsx
+++ travel/app/context/AuthContext.tsx
@@ -1,194 +1,223 @@
-import { createContext, useContext, useEffect, useState } from 'react'; import axios from 'axios';
-import * as SecureStore from 'expo-secure-store';
-import { router } from 'expo-router';
-import LoadingIndicator from '@/components/LoadingIndicator';
-
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+import { router } from "expo-router";
+import LoadingIndicator from "@/components/LoadingIndicator";
+import messaging from "@react-native-firebase/messaging";
 
 interface AuthProps {
-    authState?: { token: string | null; authenticated: boolean | null };
-    onRegister?: (email: string, password: string, username: string, birth_date: string, preferences: string[], city: string) => Promise<any>;
-    onLogin?: (email: string, password: string) => Promise<any>;
-    onRefreshToken?: () => Promise<any>;
-    onLogout?: () => Promise<any>;
+  authState?: { token: string | null; authenticated: boolean | null };
+  onRegister?: (
+    email: string,
+    password: string,
+    username: string,
+    birth_date: string,
+    preferences: string[],
+    city: string
+  ) => Promise<any>;
+  onLogin?: (email: string, password: string) => Promise<any>;
+  onRefreshToken?: () => Promise<any>;
+  onLogout?: () => Promise<any>;
 }
-
 
 const TOKEN_KEY = "my-jwt";
 const REFRESH_TOKEN_KEY = "refresh-token";
 export const API_URL = "https://api-gateway-e26h.onrender.com";
 const AuthContext = createContext<AuthProps>({});
 
-
 export const useAuth = () => {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 };
 
 export const AuthProvider = ({ children }: any) => {
-    const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-    const [authState, setAuthState] = useState<{
-        token: string | null;
-        refresh_token: string | null;
-        authenticated: boolean | null;
-    }>({
-        token: null,
-        refresh_token: null,
-        authenticated: null,
-    })
+  const [authState, setAuthState] = useState<{
+    token: string | null;
+    refresh_token: string | null;
+    authenticated: boolean | null;
+  }>({
+    token: null,
+    refresh_token: null,
+    authenticated: null,
+  });
 
-    useEffect(() => {
-        const loadToken = async () => {
-            setLoading(true)
-            let token = null;
-            let refresh_token = null;
-            try {
-                token = await SecureStore.getItemAsync(TOKEN_KEY);
-            } catch (e) {
-                token = null
-            }
-            try {
-                refresh_token = await SecureStore.getItemAsync(REFRESH_TOKEN_KEY);
-            } catch (e) {
-                refresh_token = null
-            }
+  useEffect(() => {
+    const loadToken = async () => {
+      setLoading(true);
+      let token = null;
+      let refresh_token = null;
+      try {
+        token = await SecureStore.getItemAsync(TOKEN_KEY);
+      } catch (e) {
+        token = null;
+      }
+      try {
+        refresh_token = await SecureStore.getItemAsync(REFRESH_TOKEN_KEY);
+      } catch (e) {
+        refresh_token = null;
+      }
 
-            console.log("stored:", token);
-            console.log("refresh:", refresh_token);
-            if (token != null && refresh_token != null) {
-                axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-                try {
-                    await axios.get(`${API_URL}/users/verify_id_token`);
-                    router.replace("/(tabs)")
-                } catch (e) {
-                    axios.defaults.headers.common['Authorization'] = `Bearer ${refresh_token}`;
-                    console.log(axios.defaults.headers.common['Authorization'])
-                    try {
-                        const result = await axios.post(`${API_URL}/users/refresh_token`);
-                        setAuthState({
-                            token: result.data.token,
-                            refresh_token: result.data.refresh_token,
-                            authenticated: true,
-                        });
-                        axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.token}`;
-                        try {
-                            await SecureStore.setItemAsync(TOKEN_KEY, result.data.token);
-                            await SecureStore.setItemAsync(REFRESH_TOKEN_KEY, result.data.refresh_token);
-                            console.log("Refreshed token")
-                            router.replace("/(tabs)")
-                        } catch (e) {
-                            alert(e)
-                        }
-                    } catch (e) {
-                        alert(e)
-                        setLoading(false)
-                    }
-                }
-            }
-            setLoading(false)
-        }
-        loadToken();
-    }, []);
-
-    const register = async (email: string, password: string, username: string, birth_date: string, preferences: string[], city: string) => {
-        console.log(`${API_URL}/users/signup`)
+      console.log("stored:", token);
+      console.log("refresh:", refresh_token);
+      if (token != null && refresh_token != null) {
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
         try {
-            return await axios.post(`${API_URL}/users/signup`, {
-                "username": username,
-                "email": email,
-                "password": password,
-                "preferences": preferences,
-                "birth_date": birth_date,
-                "city": city
-            });
+          await axios.get(`${API_URL}/users/verify_id_token`);
+          router.replace("/(tabs)");
         } catch (e) {
-            return { error: true, code: (e as any).response.status };
-        }
-    }
-
-    const login = async (email: string, password: string) => {
-        try {
-            const result = await axios.post(`${API_URL}/users/login`, { email, password });
+          axios.defaults.headers.common[
+            "Authorization"
+          ] = `Bearer ${refresh_token}`;
+          console.log(axios.defaults.headers.common["Authorization"]);
+          try {
+            const result = await axios.post(`${API_URL}/users/refresh_token`);
             setAuthState({
-                token: result.data.token,
-                refresh_token: result.data.refresh_token,
-                authenticated: true,
+              token: result.data.token,
+              refresh_token: result.data.refresh_token,
+              authenticated: true,
             });
-
-            axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.token}`;
-
+            axios.defaults.headers.common[
+              "Authorization"
+            ] = `Bearer ${result.data.token}`;
             try {
-                await SecureStore.setItemAsync(TOKEN_KEY, result.data.token);
-                await SecureStore.setItemAsync(REFRESH_TOKEN_KEY, result.data.refresh_token);
+              await SecureStore.setItemAsync(TOKEN_KEY, result.data.token);
+              await SecureStore.setItemAsync(
+                REFRESH_TOKEN_KEY,
+                result.data.refresh_token
+              );
+              console.log("Refreshed token");
+              router.replace("/(tabs)");
             } catch (e) {
-                console.log(e)
+              alert(e);
             }
-
-
-            return result;
-
-        } catch (e) {
-            return { error: true, msg: (e as any).response.data };
+          } catch (e) {
+            alert(e);
+            setLoading(false);
+          }
         }
-    }
-
-    const refreshToken = async () => {
-        try {
-            await axios.get(`${API_URL}/users/verify_id_token`);
-
-        } catch (e) {
-            try {
-                const refresh_token = await SecureStore.getItemAsync(REFRESH_TOKEN_KEY);
-                axios.defaults.headers.common['Authorization'] = `Bearer ${refresh_token}`;
-                const result = await axios.post(`${API_URL}/users/refresh_token`);
-                setAuthState({
-                    token: result.data.token,
-                    refresh_token: result.data.refresh_token,
-                    authenticated: true,
-                });
-                axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.token}`;
-                await SecureStore.setItemAsync(TOKEN_KEY, result.data.token);
-                await SecureStore.setItemAsync(REFRESH_TOKEN_KEY, result.data.refresh_token);
-                console.log("Refreshed token")
-            } catch (e) {
-                console.log(e)
-            }
-        }
-        return
-    }
-
-    const logout = async () => {
-        await SecureStore.deleteItemAsync(TOKEN_KEY);
-
-        axios.defaults.headers.common['Authorization'] = "";
-
-        setAuthState({
-            token: null,
-            refresh_token: null,
-            authenticated: false,
-        });
-    }
-
-    const value = {
-        onRegister: register,
-        onLogin: login,
-        onLogout: logout,
-        onRefreshToken: refreshToken,
-        authState
+      }
+      setLoading(false);
     };
+    loadToken();
+  }, []);
 
-    return (
-        <>
-            <AuthContext.Provider value={value}>
-                {children}
-            </AuthContext.Provider>
-            {loading && (
-                <LoadingIndicator />
-            )}
-        </>
-    );
+  const register = async (
+    email: string,
+    password: string,
+    username: string,
+    birth_date: string,
+    preferences: string[],
+    city: string
+  ) => {
+    await messaging().registerDeviceForRemoteMessages();
+
+    const token = await messaging().getToken();
+    console.log(token);
+
+    console.log(`${API_URL}/users/signup`);
+    try {
+      return await axios.post(`${API_URL}/users/signup`, {
+        username: username,
+        email: email,
+        password: password,
+        preferences: preferences,
+        birth_date: birth_date,
+        city: city,
+        fcm_token: token,
+      });
+    } catch (e) {
+      return { error: true, code: (e as any).response.status };
+    }
+  };
+
+  const login = async (email: string, password: string) => {
+    try {
+      const result = await axios.post(`${API_URL}/users/login`, {
+        email,
+        password,
+      });
+      setAuthState({
+        token: result.data.token,
+        refresh_token: result.data.refresh_token,
+        authenticated: true,
+      });
+
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${result.data.token}`;
+
+      try {
+        await SecureStore.setItemAsync(TOKEN_KEY, result.data.token);
+        await SecureStore.setItemAsync(
+          REFRESH_TOKEN_KEY,
+          result.data.refresh_token
+        );
+      } catch (e) {
+        console.log(e);
+      }
+
+      return result;
+    } catch (e) {
+      return { error: true, msg: (e as any).response.data };
+    }
+  };
+
+  const refreshToken = async () => {
+    try {
+      await axios.get(`${API_URL}/users/verify_id_token`);
+    } catch (e) {
+      try {
+        const refresh_token = await SecureStore.getItemAsync(REFRESH_TOKEN_KEY);
+        axios.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${refresh_token}`;
+        const result = await axios.post(`${API_URL}/users/refresh_token`);
+        setAuthState({
+          token: result.data.token,
+          refresh_token: result.data.refresh_token,
+          authenticated: true,
+        });
+        axios.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${result.data.token}`;
+        await SecureStore.setItemAsync(TOKEN_KEY, result.data.token);
+        await SecureStore.setItemAsync(
+          REFRESH_TOKEN_KEY,
+          result.data.refresh_token
+        );
+        console.log("Refreshed token");
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    return;
+  };
+
+  const logout = async () => {
+    await SecureStore.deleteItemAsync(TOKEN_KEY);
+
+    axios.defaults.headers.common["Authorization"] = "";
+
+    setAuthState({
+      token: null,
+      refresh_token: null,
+      authenticated: false,
+    });
+  };
+
+  const value = {
+    onRegister: register,
+    onLogin: login,
+    onLogout: logout,
+    onRefreshToken: refreshToken,
+    authState,
+  };
+
+  return (
+    <>
+      <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+      {loading && <LoadingIndicator />}
+    </>
+  );
 };
-
-
-
-
-
--- travel/app/index.tsx
+++ travel/app/index.tsx
@@ -1,45 +1,81 @@
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image } from "react-native";
 
-import { Text, View } from '@/components/Themed';
-import { useRouter } from 'expo-router';
-import AccountButton from '@/components/AccountButton';
+import { Text, View } from "@/components/Themed";
+import { useRouter } from "expo-router";
+import AccountButton from "@/components/AccountButton";
+import messaging from "@react-native-firebase/messaging";
+import notifee from '@notifee/react-native';
+
 
 export default function InitialScreen() {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <View style={styles.container}>
-          <View style={{flex:1}}>
-            <Text style={styles.title}>Travel App</Text>
-            <Image 
-            style={{width:200, height:200, alignSelf:'center', opacity:0.8, marginTop:20}}
-            source={{
-              uri:"https://i.imgur.com/qc0GM7G.png"}}
-              />
-          </View>
-          <View>
-            <AccountButton title="Log In" onPress={() => router.navigate("user/login")}/>
-            <AccountButton title="Sign Up" onPress={() => router.navigate("user/signup")}/>
-          </View>
-        </View>
-    );
+  
+  
+
+  async function onMessageReceived(message: any) {
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+    notifee.displayNotification({
+      title: message.notification.title,
+      body: message.notification.body,
+      android: {
+        channelId,
+      },
+    });
+  }
+
+
+  messaging().onMessage(onMessageReceived);
+
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 1 }}>
+        <Text style={styles.title}>Travel App</Text>
+        <Image
+          style={{
+            width: 200,
+            height: 200,
+            alignSelf: "center",
+            opacity: 0.8,
+            marginTop: 20,
+          }}
+          source={{
+            uri: "https://i.imgur.com/qc0GM7G.png",
+          }}
+        />
+      </View>
+      <View>
+        <AccountButton
+          title="Log In"
+          onPress={() => router.navigate("user/login")}
+        />
+        <AccountButton
+          title="Sign Up"
+          onPress={() => router.navigate("user/signup")}
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical:70
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 70,
   },
   title: {
     fontSize: 40,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
-    marginTop:280
+    width: "80%",
+    marginTop: 280,
   },
 });
